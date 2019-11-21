89e59e884feecda9705c6ba901c395f5bc5a0552
Nour Sofanati <ultradraftns@gmail.com>
2019-11-21 11:15:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let timeList = [
 
 //creating a function to determine local time and dynamically generate HTML for a table
 function dynamicTime() {
-    for (let i=0; i<timeList.length; i++) {
+    for (let i = 0; i < timeList.length; i++) {
         let tableRow = $("<tr></tr>").attr("id", i);
         //timeBlock --> contains timeList elements
         let timeBlock = $("<td></td>");
@@ -27,15 +27,21 @@ function dynamicTime() {
         let description = $("<td></td>");
         //utilizing bootstrap to make website device responsive
         description.addClass("col-md-12");
-        let textarea = $("<textarea></textarea>");
+        let textarea = $("<textarea></textarea>").attr("id", `t-${i}`);
         textarea.addClass("textarea form-control");
+        textarea.val(localStorage.getItem(`t-${i}`));
         description.append(textarea);
         //saveButtons generated for storing user data to local storage as desired
         let saveButton = $("<td></td>");
-        let button = $("<button></button>");
-        //add event listener click event 
-        button.addClass("saveBtn");
+        let button = document.createElement('button')
+            //add event listener click event 
+        button.classList.add("saveBtn");
         saveButton.append(button);
+        button.onclick = () => {
+            localStorage.setItem(`t-${i}`, $(`#t-${i}`).val())
+            console.log($(`#t-${i}`).val());
+        }
+
         //appending all td elements to each row of the table
         tableRow.append(timeBlock, description, saveButton);
         //appending each row of the table sequentially to the table itself 
